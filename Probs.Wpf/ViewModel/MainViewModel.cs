21e8784cafa0bed1370b707539f4f2f6d2a0ff7a
Adam Blair <adam.blair@systemc.com>
2017-05-19 14:25:30 +0100

    Added in probs data series

--- Probs.Wpf/ViewModel/MainViewModel.cs
+++ Probs.Wpf/ViewModel/MainViewModel.cs
@@ -1,27 +1,52 @@
 using GalaSoft.MvvmLight;
 using LiveCharts;
 using LiveCharts.Wpf;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Probs.Wpf.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        readonly ProbabilityGenerator _probabilityGenerator;
+        readonly int[] _tnList;
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
-            this.Series = new SeriesCollection
+            _probabilityGenerator = new ProbabilityGenerator();
+            _tnList = new int[] { 8, 10, 12, 14, 16, 18 };
+
+            this.Series = MakeSeriesCollection();
+        }
+
+        SeriesCollection MakeSeriesCollection()
+        {
+            var seriesList = new SeriesCollection();
+
+            var dice = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+
+            IEnumerable<LineSeries> series = dice.Select(MakeLineSeries);
+
+            var x = new SeriesCollection();
+
+            x.AddRange(series);
+
+            return x;
+        }
+
+        LineSeries MakeLineSeries(int dice)
+        {
+            double[] successProbability = _probabilityGenerator.GetProbabilities(dice, _tnList);
+
+            var series = new LineSeries
             {
-                new LineSeries
-                {
-                    Values = new ChartValues<double> { 3, 5, 7, 4 }
-                },
-                new ColumnSeries
-                {
-                    Values = new ChartValues<decimal> { 5, 6, 2, 7 }
-                }
+                Values = new ChartValues<double>(successProbability)
             };
+
+            return series;
         }
 
         public SeriesCollection Series { get; set; }
