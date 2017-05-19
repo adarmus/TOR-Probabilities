412e516180de8ba277b06c933e9f93727f7fea10
Adam Blair <adam.blair@systemc.com>
2017-05-19 14:07:18 +0100

    Added MvvmLight and LiveCharts.Wpf packages

---
+++ Probs.Wpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,14 @@
+using GalaSoft.MvvmLight;
+
+namespace Probs.Wpf.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+        }
+    }
+}
